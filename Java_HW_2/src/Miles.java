80cc8e5978077e65077b102f1d1bdfad5e4e8f51
Ekaterina Shilova <ekaterinalstmpr@gmail.com>
2021-09-15 16:11:14 +0300

    Add java and gitignore

---
+++ Java_HW_2/src/Miles.java
@@ -0,0 +1,10 @@
+public class Miles {
+
+    public static void main(String[] args) {
+        int ticketCost = 150;
+        int cost1mile = 20;
+        int countBonus = ticketCost / cost1mile;
+        System.out.println(countBonus);
+    }
+}
+
